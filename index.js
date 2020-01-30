a91934837ca4892cb5ef02b03c2127f2727f6962
Faisal Manzer <faisal_manzer@yahoo.in>
2020-01-31 03:13:03 +0530

    Added more tests Bumped version

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-let postcss = require('postcss')
+let postcss = require('postcss');
 
 module.exports =
-postcss.plugin('postcss-viewport-height-correction', () => {
-  let finderRegex = /([0-9.]+)vh/
-  let excludeRegex = /--vh/
-  let replaceBy = 'calc(var(--vh, 1vh) * $1)'
+  postcss.plugin('postcss-viewport-height-correction', () => {
+    let finderRegex = /([0-9.]+)vh/;
+    let excludeRegex = /--vh/;
+    let replaceBy = 'calc(var(--vh, 1vh) * $1)';
 
-  return function (root) {
-    /* Rules are the CSS Block */
-    root.walkRules(rule => {
-      /* Decls are the properties inside */
-      rule.walkDecls(decl => {
-        let value = decl.value
+    return function (root) {
+      /* Rules are the CSS Block */
+      root.walkRules(rule => {
+        /* Decls are the properties inside */
+        rule.walkDecls(decl => {
+          let value = decl.value;
 
-        // Checking that declaration has `vh` and is not corrcted.
-        if (value.match(finderRegex) !== null && value.match(excludeRegex) === null) {
-          let correctedViewport = value.replace(finderRegex, replaceBy)
+          // Checking that declaration has `vh` and is not corrected.
+          let isMatch = value.match(finderRegex) !== null;
+          let isPreParsed = value.match(excludeRegex) === null;
+          if (isMatch && isPreParsed) {
+            let correctedViewport = value.replace(finderRegex, replaceBy);
 
-          // Appending because we want to preserve
-          // the main property for fallback
-          rule.append({
-            prop: decl.prop,
-            value: correctedViewport
-          })
-        }
+            // Appending because we want to preserve
+            // the main property for fallback
+            rule.append({
+              prop: decl.prop,
+              value: correctedViewport
+            })
+          }
+        })
       })
-    })
-  }
-})
+    }
+  });
--- index.test.js
+++ index.test.js
@@ -1,17 +1,34 @@
-let postcss = require('postcss')
+let postcss = require('postcss');
 
-let plugin = require('./')
+let plugin = require('./');
 
 async function run (input, output, opts) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
+  expect(result.css).toEqual(output);
   expect(result.warnings()).toHaveLength(0)
 }
 
-it('does something', async () => {
+it('changes vh', async () => {
   await run(
     'a{ height: 10vh; color: #FF10GG; }',
     'a{ height: 10vh; color: #FF10GG; height: calc(var(--vh, 1vh) * 10); }',
-    { }
+    {}
   )
-})
+});
+
+it('does not have any change in priority', async () => {
+  await run(
+    'a{ height: 100px; height: 10vh; color: #FF10GG; }',
+    'a{ height: 100px; height: 10vh; ' +
+    'color: #FF10GG; height: calc(var(--vh, 1vh) * 10); }',
+    {}
+  )
+});
+
+it('does not have any effect on parsed', async () => {
+  await run(
+    'a{ height: calc(var(--vh, 1vh) * 10); color: #FF10GG; }',
+    'a{ height: calc(var(--vh, 1vh) * 10); color: #FF10GG; }',
+    {}
+  )
+});
